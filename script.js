b4b4822f71af85cdf340897c807376b8fbf6f77b
madara-uchiha-76 <mu0278324@gmail.com>
2026-08-29 21:16:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+```javascript
+function startLearning() {
+    document.getElementById("learn").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+
+function showDemo() {
+    alert(
+        "Welcome to HolaLearn! 🇪🇸\n\n" +
+        "You can learn Vocabulary, Grammar, " +
+        "Pronunciation and Conversation."
+    );
+}
+
+
+function openLesson(lesson) {
+    alert(
+        "You selected: " + lesson + "\n\n" +
+        "This lesson section will be added in the next part!"
+    );
+}
+
+
+function startPractice() {
+    alert(
+        "Practice section coming soon! 📝\n\n" +
+        "We will build interactive Spanish exercises here."
+    );
+}
+
+
+function playWord() {
+
+    const word = new SpeechSynthesisUtterance("Hola");
+
+    word.lang = "es-ES";
+
+    speechSynthesis.speak(word);
+}
+```
